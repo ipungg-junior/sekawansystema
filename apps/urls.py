4feeb229d6ccd1007547ac65ef6bbba68aec8889
ipungg-junior <ipungg.id@gmail.com>
2023-11-04 22:29:50 +0700

    add demo-client pge

--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.views import Landing
+from apps.views import Landing, Demo
+
+handler404 = 'apps.views.entry_not_found'
 
 urlpatterns = [
     path('', Landing.as_view(context='')),
+    path('demo/pge/', Demo.as_view(context='demo-pge')),
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
+def entry_not_found(request, exception, template_name='404.html'):
+    return render(request, template_name)
+
 # Create your views here.
 class Landing(View):
     
@@ -8,3 +11,11 @@ class Landing(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
+
+class Demo(View):
+    
+    context = ''
+
+    def get(self, request):
+        return render(request, 'client-demo/pge/index.html')
